7d2356874474fe269a48d71abc863ff21d18300d
adeyinkaodebade <adeyinka.odebade.ext@bbc.co.uk>
2024-07-15 16:32:10 +0100

    Adding PII data to the data generated and removing duplicate aud_id

--- sample_data/sample_aud_data.py
+++ sample_data/sample_aud_data.py
@@ -1,21 +1,30 @@
 import numpy as np
 import polars as pl
 import time
+# import random as r
+from faker import Faker
 
 # Constants for data generation
 LOCATION_SET = ['India', 'USA', 'China', 'Russia', 'Israel', 'UK', 'Germany', 'UAE', "Poland", 'Nepal', 'Nigeria',
                 'Vietnam', 'Australia']
 DEVICE_CAT = ['Web', 'Mobile', 'TV', 'Others']
 VIDEO_CAT_SET = ["TV", "Movies", "Trailer", "Music", "Tech", "Review", "Drama"]
-NUM_ROWS = 10000
+NUM_ROWS = 100
 SEED = 47
+
+file_path = f"data/aud_data{time.strftime('%Y%m%d_%H%M%S')}"
+rng = np.random.default_rng(seed=SEED)
+fake = Faker()
 
 
 # Function to generate data
 def generate_data(num_rows):
-    rng = np.random.default_rng(seed=SEED)
     data = {
-        "aud_id": rng.integers(low=1, high=num_rows, size=num_rows),
+        "aud_id": np.arange(1, num_rows + 1),  #rng.integers(low=1, high=num_rows, size=num_rows),
+        "first_name": [fake.first_name() for _ in range(num_rows)],
+        "last_name": [fake.last_name() for _ in range(num_rows)],
+        "email": [fake.email() for _ in range(num_rows)],
+        "phone_number": [fake.phone_number() for _ in range(num_rows)],
         "age": rng.integers(low=10, high=80, size=num_rows),
         "location": rng.choice(LOCATION_SET, size=num_rows),
         "dev_cat": rng.choice(DEVICE_CAT, size=num_rows),
@@ -26,13 +35,12 @@ def generate_data(num_rows):
 
 # Save data to CSV
 def save_data(df, file_path):
-    df.write_csv(file_path)
-    df.write_parquet(file_path)
+    df.write_csv(file_path + '.csv')
+    df.write_parquet(file_path + '.parquet')
     print(f"Data saved to {file_path}")
 
 
 if __name__ == "__main__":
-    file_path = f"../data/aud_data{time.strftime('%Y%m%d_%H%M%S')}.csv"
     aud_data = generate_data(NUM_ROWS)
     save_data(aud_data, file_path)
     print(aud_data)
